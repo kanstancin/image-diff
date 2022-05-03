1a86a8a526de84bc5c96c01750fd3dd0dc08877d
kanstancin <ceitmo@gmail.com>
2022-05-03 17:15:39 -0400

    upd

--- img_diff_c9.py
+++ img_diff_c9.py
@@ -7,14 +7,14 @@ from scipy import stats
 
 
 def normalize_img(img):
-    assert(len(img.shape) == 2)
+    assert (len(img.shape) == 2)
     img = img - np.min(img)
     img = ((img / np.max(img)) * 255).astype(np.uint8)
     return img
 
 
 def normalize_img2(inp):
-    assert(len(inp.shape) == 2)
+    assert (len(inp.shape) == 2)
     print(np.min(inp), np.max(inp))
     mean, _ = stats.mode(inp, axis=None)
     print(mean)
@@ -32,69 +32,148 @@ def normalize_img3(img):
     # img = ((img / np.max(img)) * 255).astype(np.uint8)
     return img
 
+
 def getAOIMask(img_shape, poly_pts):
     assert len(pts.shape) == 3, "poly shape should be 3"
     mask = np.zeros((img_shape[0:2]), dtype=np.uint8)
-    mask2 = np.zeros((img_shape[0]+2, img_shape[1]+2), dtype=np.uint8)
-    cv.polylines(mask, [poly_pts], False, 255)
-    _, mask, _, _ = cv.floodFill(mask, mask2, (0, 0), 255)
+    mask2 = np.zeros((img_shape[0] + 2, img_shape[1] + 2), dtype=np.uint8)
+    cv.polylines(mask, [poly_pts], True, 255)
+    fill_pt = (int(img_shape[1]/2), int(img_shape[0]/2))
+    _, mask, _, _ = cv.floodFill(mask, mask2, fill_pt, 255)
+    mask = cv.bitwise_not(mask)
     mask[mask == 1] = 255
     print(mask.dtype)
     return mask
 
 
-inp_path = '/home/cstar/workspace/grid-data/G10-Z50-D500-0/'
-
-img = cv.imread(os.path.join(inp_path, 'img_X211.00_Y345.00_Z50.00.jpg'), 1)
-img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-kernel = (15, 15)
-img_hsv = cv.GaussianBlur(img_hsv, kernel, cv.BORDER_DEFAULT).astype(np.int32)
-
-# fig, ax = plt.subplots(2, 2)
-# ax[0, 0].imshow(img)
-# ax[0, 1].imshow(img_hsv[:, :, 0], cmap='gray')
-# ax[1, 0].imshow(img_hsv[:, :, 1])
-# ax[1, 1].imshow(img_hsv[:, :, 2])
-# plt.show()
-
-img_2 = cv.imread(os.path.join(inp_path, 'img_X43.50_Y345.00_Z50.00.jpg'), 1) #img_X43.50_Y345.00_Z50.00.jpg #img_X177.50_Y144.00_Z50.00
-img_hsv_2 = cv.cvtColor(img_2, cv.COLOR_BGR2HSV)
-img_hsv_2 = cv.GaussianBlur(img_hsv_2, kernel, cv.BORDER_DEFAULT).astype(np.int32)
-
-h_diff = img_hsv_2[:, :, 0] - img_hsv[:, :, 0]
-h_diff = normalize_img3(h_diff)
-
-s_diff = img_hsv_2[:, :, 1] - img_hsv[:, :, 1]
-s_diff = normalize_img3(s_diff)
-
-v_diff = img_hsv_2[:, :, 2] - img_hsv[:, :, 2]
-# histogram, bin_edges = np.histogram(v_diff, bins=100)
-# plt.plot(bin_edges[0:-1], histogram)  # <- or here
-# plt.show()
+def prepropImg(img, kernel=(15, 15)):
+    img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    img = cv.GaussianBlur(img, kernel, cv.BORDER_DEFAULT).astype(np.int32)
+    return img
 
 
+def getImDiffMask(im1, im2, maskAOI=None, method="saturation"):
+    im1_hsv = prepropImg(im1, kernel=(7, 7))
+    im2_hsv = prepropImg(im2, kernel=(7, 7))
 
-fig, ax = plt.subplots(2, 2)
-ax[0, 0].imshow(img_2)
-ax[0, 1].imshow(img_hsv_2[:, :, 0], cmap='gray')
-ax[1, 0].imshow(img_hsv_2[:, :, 1], cmap='gray')
-ax[1, 1].imshow(img_hsv_2[:, :, 2], cmap='gray')
-plt.show()
+    h_diff = im2_hsv[:, :, 0] - im1_hsv[:, :, 0]
+    h_diff = normalize_img3(h_diff)
 
-pts = np.array([[0, 347], [504, 166], [1075, 261], [1008, 719]])
-cv.polylines(s_diff, [pts], True, 255)
-plt.imshow(s_diff)
-plt.show()
+    s_diff = im2_hsv[:, :, 1] - im1_hsv[:, :, 1]
+    s_diff = normalize_img3(s_diff)
+
+    v_diff = im2_hsv[:, :, 2] - im1_hsv[:, :, 2]
+    v_diff = normalize_img3(v_diff)
+
+    # to process
+    base_ch = s_diff.copy()
+    plt.title("base_ch before maskAOI")
+    vmax = 100
+    plt.imshow(base_ch, cmap='gray', vmin=0, vmax=vmax)
+    plt.show()
+    # apply ROI mask
+    base_ch[maskAOI == 255] = 0
+    plt.title("base_ch after maskAOI")
+    plt.imshow(base_ch, cmap='gray', vmin=0, vmax=vmax)
+    plt.show()
+
+    # plot 2 selected images
+    # plot hsv
+    fig, ax = plt.subplots(1, 2)
+    fig.suptitle('Selected images: im1, im2', fontsize=16)
+    ax[0].imshow(cv.cvtColor(im1, cv.COLOR_BGR2RGB))
+    ax[1].imshow(cv.cvtColor(im2, cv.COLOR_BGR2RGB))
+    plt.show()
+
+    # plot hsv
+    fig, ax = plt.subplots(2, 2)
+    fig.suptitle('Im2, HSV plots', fontsize=16)
+    ax[0, 0].imshow(cv.cvtColor(im2, cv.COLOR_BGR2RGB))
+    ax[0, 1].imshow(im2_hsv[:, :, 0], cmap='gray')
+    ax[1, 0].imshow(im2_hsv[:, :, 1], cmap='gray')
+    ax[1, 1].imshow(im2_hsv[:, :, 2], cmap='gray')
+    plt.show()
+
+    # plot hsv diff
+    fig, ax = plt.subplots(2, 2)
+    fig.suptitle('Im2, HSV diff plots', fontsize=16)
+    ax[0, 0].imshow(cv.cvtColor(im2, cv.COLOR_BGR2RGB))
+    ax[0, 1].imshow(h_diff, cmap='gray')
+    ax[1, 0].imshow(s_diff, cmap='gray')
+    ax[1, 1].imshow(v_diff, cmap='gray')
+    plt.show()
+
+    # create the histogram, plot #1
+    histogram, bin_edges = np.histogram(base_ch, bins=256, range=(0, 255))
+    plt.figure()
+    plt.title("Image Difference Histogram, #1")
+    plt.xlabel("Intensity")
+    plt.ylabel("Count")
+    plt.ylim([0, 5000])  # <- named arguments do not work here
+    plt.plot(bin_edges[0:-1], histogram)  # <- or here
+    plt.show()
+    # create the histogram, plot #2
+    plt.ylim([0, 5000])
+    plt.hist(base_ch.reshape(-1), bins=256, range=(0, 255))
+    plt.title("hist #2")
+    plt.show()
+
+    # threshold diff
+    threshold = 10
+    mask_diff = np.zeros(base_ch.shape)
+    mask_diff[base_ch > threshold] = 255
+    print(mask_diff.shape)
+    mask_diff = mask_diff.astype("uint8")
+    kernel = np.ones((5, 5), np.uint8)
+    # mask_diff = cv.erode(mask_diff, kernel, iterations=1)
+    plt.title("mask value after threshold")
+    plt.imshow(mask_diff, cmap='gray')
+    plt.show()
+
+    return mask_diff
+
+
+def is_inp(img_name):
+    return img_name.endswith(("jpg", "jpeg", "png"))
+
+
+def get_img_paths(im_in_dir, verbose=True):
+    all_inps = os.listdir(im_in_dir)
+    all_inps = [name for name in all_inps if is_inp(name)]
+    if verbose:
+        print('*'*33)
+        print("Found the following {} images:".format(len(all_inps)))
+        for im_path in all_inps:
+            print(im_path)
+        print('*'*33)
+    return all_inps
+
+
+inp_path_spag = '/home/cstar/workspace/grid-data/dataset-im-diff-no-shadows-Z30/images/train/'
+inp_img_spag_paths = get_img_paths(inp_path_spag)
+inp_path_nospag = '/home/cstar/workspace/grid-data/im-test-no-shadow/'
+inp_img_nospag_paths = get_img_paths(inp_path_nospag)
+i1, i2 = (0, 20)
+im1_path = os.path.join(inp_path_nospag, inp_img_nospag_paths[i1])
+im2_path = os.path.join(inp_path_spag, inp_img_spag_paths[i2])
+print(f"input imgs: \n\t {inp_img_nospag_paths[i1]} \n\t {inp_img_spag_paths[i2]}")
+
+
+im1 = cv.imread(os.path.join(inp_path_nospag, im1_path), 1)
+im2 = cv.imread(os.path.join(inp_path_spag, im2_path),
+                  1)  # img_X43.50_Y345.00_Z50.00.jpg #img_X177.50_Y144.00_Z50.00
+
+# TODO: func img, img, maskAOI -> mask of diff
+
+# create ROI mask
+pts = np.array([[0, 347], [504, 166], [1075, 261], [1008, 719], [683, 719], [0, 356]])
 pts = pts.reshape((-1, 1, 2))
-mask = getAOIMask(img_shape=img.shape, poly_pts=pts)
-plt.imshow(mask)
-plt.title("mask")
+maskAOI = getAOIMask(img_shape=im1.shape, poly_pts=pts)
+plt.imshow(maskAOI)
+plt.title("maskAOI")
 plt.show()
 
-
-
-
-v_diff = normalize_img3(v_diff)
+# resize
 # width = 1000
 # height = int(width / v_diff.shape[1] * v_diff.shape[0])
 # print(np.amin(v_diff), np.amax(v_diff))
@@ -104,32 +183,7 @@ v_diff = normalize_img3(v_diff)
 # img = cv.resize(img_2, shape, interpolation=cv.INTER_AREA)
 # mask = cv.resize(mask, shape, interpolation=cv.INTER_NEAREST)
 
-# create the histogram
-histogram, bin_edges = np.histogram(v_diff, bins=256, range=(0, 255))
-
-plt.figure()
-plt.title("Image Difference Histogram")
-plt.xlabel("Intensity")
-plt.ylabel("Count")
-# plt.xlim([0.0, 1.0])  # <- named arguments do not work here
-plt.plot(bin_edges[0:-1], histogram)  # <- or here
-plt.show()
-
-plt.hist(v_diff.reshape(-1), bins=256)
-plt.title("value hist")
-plt.show()
-
-threshold = 60
-mask = np.zeros(v_diff.shape)
-mask[v_diff > threshold] = 255
-print(mask.shape)
-mask = mask.astype("uint8")
-kernel = np.ones((5, 5), np.uint8)
-# mask = cv.erode(mask, kernel, iterations=1)
-plt.title("mask value after threshold")
-plt.imshow(mask, cmap='gray')
-plt.show()
-
+# plot difference
 # fig, ax = plt.subplots(2, 2)
 # ax[0, 0].imshow(img_2)
 # ax[0, 1].imshow(h_diff, cmap='gray')
@@ -137,23 +191,17 @@ plt.show()
 # ax[1, 1].imshow(v_diff, cmap='gray')
 # plt.show()
 
-img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-plt.imshow(img, cmap='gray')
-plt.title("value difference")
-plt.show()
+mask_diff = getImDiffMask(im1, im2, maskAOI=maskAOI, method="saturation")
 
 ROI_number = 0
-cnts = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+cnts = cv.findContours(mask_diff, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 for c in cnts:
     if len(c) < 100:
         continue
     print(len(c))
-    x,y,w,h = cv.boundingRect(c)
-    cv.rectangle(img, (x, y), (x + w, y + h), (0,0,255), 2)
-plt.title("input img with detections")
-plt.imshow(img)
+    x, y, w, h = cv.boundingRect(c)
+    cv.rectangle(im2, (x, y), (x + w, y + h), (0, 0, 255), 2)
+plt.title("input im2 with detections")
+plt.imshow(cv.cvtColor(im2, cv.COLOR_BGR2RGB))
 plt.show()
-
-
-
